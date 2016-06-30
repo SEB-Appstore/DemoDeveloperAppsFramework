04389bb2fa597bc20e810727e9d63cb75ac64e3f
SEB <seb@seb-appstore.com>
2016-07-01 00:27:50 +0700

    Initial Commit

---
+++ DemoDeveloperAppsFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoDeveloperAppsFramework
+//
+//  Created by SEB on 7/1/16.
+//  Copyright © 2016 SEB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoDeveloperAppsFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoDeveloperAppsFramework
+//
+//  Created by SEB on 7/1/16.
+//  Copyright © 2016 SEB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
